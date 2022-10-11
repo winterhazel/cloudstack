ffb66782c37138a6ae9342b1d16a7a6a36f2bd46
Daniel Augusto Veronezi Salvador <daniel@scclouds.com.br>
2022-10-11 11:15:09 -0300

    Fix

--- framework/quota/src/main/java/org/apache/cloudstack/quota/constant/QuotaConfig.java
+++ framework/quota/src/main/java/org/apache/cloudstack/quota/constant/QuotaConfig.java
@@ -52,7 +52,7 @@ public interface QuotaConfig {
             "Sender of quota alert email (will be in the From header of the email).", true);
 
     public static final ConfigKey<String> QuotaSmtpEnabledSecurityProtocols = new ConfigKey<String>("Advanced", String.class, "quota.usage.smtp.enabledSecurityProtocols", "",
-            "White-space separated security protocols; ex: \"TLSv1 TLSv1.1\". Supported protocols: SSLv2Hello, SSLv3, TLSv1, TLSv1.1 and TLSv1.2.", true);
+            "White-space separated security protocols; ex: \"TLSv1 TLSv1.1\". Supported protocols: SSLv2Hello, SSLv3, TLSv1, TLSv1.1, and TLSv1.2", true);
 
     public static final ConfigKey<String> QuotaSmtpUseStartTLS = new ConfigKey<String>("Advanced", String.class, "quota.usage.smtp.useStartTLS", "false",
             "If set to true and if we enable security via quota.usage.smtp.useAuth, this will enable StartTLS to secure the connection.", true);
